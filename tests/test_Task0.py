f1bbd907cda97fe218e0b05cf3fdf84006d3c69d
Angelica <Angelica137@users.noreply.github.com>
2021-08-03 21:34:04 +0100

    task0, print first text formatted data, red

--- tests/test_Task0.py
+++ tests/test_Task0.py
@@ -7,5 +7,5 @@ with open('scripts/texts.csv', 'r') as f:
 
 
 def test_firstText_returns_first_text():
-    assert firstText(texts) == ['97424 22395',
-                                '90365 06212', '01-09-2016 06:03:22']
+    assert firstText(
+        texts) == "First record of texts, 97424 22395 texts 90365 06212 at time 01-09-2016 06: 03: 22"
