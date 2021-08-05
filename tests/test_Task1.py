1b6df88295960d04820fd4a57cc2561b74765594
Angelica <Angelica137@users.noreply.github.com>
2021-08-05 20:33:30 +0100

    delete obsolete tests

--- tests/test_Task1.py
+++ tests/test_Task1.py
@@ -6,19 +6,6 @@ texts2 = [[1, 1, 'bla'], [2, 1, 'bla']]
 texts3 = [[1, 3, 'bla'], [2, 1, 'bla'], [1, 3, 'bla']]
 texts4 = [[1, 3, 'bla'], [2, 1, 'bla'], [2, 1, 'bla'], [1, 3, 'bla']]
 
-'''
-def test_countTextUsers_retutns_1():
-    assert countTextUsers(texts1) == [1]
-
-
-def test_countTextUsers_returns_1_and_2():
-    assert countTextUsers(texts2) == [1, 2]
-
-
-def test_countTextusers_returns_1_3_and_2():
-    assert countTextUsers(texts3) == [1, 3, 2]
-'''
-
 
 def test_countTextusers_returns_3():
     assert countTextUsers(texts3) == 3
