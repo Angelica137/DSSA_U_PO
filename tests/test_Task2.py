b6044de0f79727385f0ea56184a6309ae6e1c9dd
Angelica <Angelica137@users.noreply.github.com>
2021-08-05 21:49:53 +0100

    task2 test with actual data, gree

--- tests/test_Task2.py
+++ tests/test_Task2.py
@@ -1,16 +1,25 @@
+import csv
 from scripts.Task2 import longestCall
 
-calls = [['78130 00821', '98453 94494', '01-09-2016 06:01:12', '186'], ['78298 91466',
-                                                                        '(022)28952819', '01-09-2016 06:01:59', '2093'], ['97424 22395', '(022)47410783', '01-09-2016 06:03:51', '1975']]
+calls1 = [['78130 00821', '98453 94494', '01-09-2016 06:01:12', '186'], ['78298 91466',
+                                                                         '(022)28952819', '01-09-2016 06:01:59', '2093'], ['97424 22395', '(022)47410783', '01-09-2016 06:03:51', '1975']]
 
 
 calls2 = [['78130 00821', '98453 94494', '01-09-2016 06:01:12', '50000'], ['78298 91466',
                                                                            '(022)28952819', '01-09-2016 06:01:59', '2093'], ['97424 22395', '(022)47410783', '01-09-2016 06:03:51', '1975']]
 
+with open('scripts/calls.csv', 'r') as f:
+    reader = csv.reader(f)
+    calls = list(reader)
 
-def test_longestCall_returns_10():
-    assert longestCall(calls) == '78298 91466'
+
+def test_longestCall_returns_calls1():
+    assert longestCall(calls1) == '78298 91466'
 
 
-def test_longestCall_returns_10():
+def test_longestCall_returns_calls2():
     assert longestCall(calls2) == '78130 00821'
+
+
+def test_longestCall_returns_10():
+    assert longestCall(calls) == '89071 50880'
