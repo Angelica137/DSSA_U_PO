3847c1d5f6264b7e8b5f3099b34b83fd84836c88
Angelica <Angelica137@users.noreply.github.com>
2021-08-05 20:27:37 +0100

    .countTextUsers returns 1, 2 and 3, red

--- tests/test_Task1.py
+++ tests/test_Task1.py
@@ -3,6 +3,7 @@ from scripts.Task1 import countTextUsers
 
 texts1 = [[1, 1, 'bla'], [1, 1, 'bla']]
 texts2 = [[1, 1, 'bla'], [2, 1, 'bla']]
+texts3 = [[1, 3, 'bla'], [2, 1, 'bla'], [1, 3, 'bla']]
 
 
 def test_countTextUsers_retutns_1():
@@ -11,3 +12,7 @@ def test_countTextUsers_retutns_1():
 
 def test_countTextUsers_returns_1_and_2():
     assert countTextUsers(texts2) == [1, 2]
+
+
+def test_countTextusers_returns_1_2_and_3():
+    assert countTextUsers(texts3) == [1, 2, 3]
