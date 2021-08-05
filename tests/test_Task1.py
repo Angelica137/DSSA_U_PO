1760c4ead8a2ca2d73292ecf8c4f0b4a0f698b48
Angelica <Angelica137@users.noreply.github.com>
2021-08-05 20:25:15 +0100

    .countTextUsers returns 1 and 2, green

--- tests/test_Task1.py
+++ tests/test_Task1.py
@@ -1,12 +1,13 @@
+from typing import Counter
 from scripts.Task1 import countTextUsers
 
-import csv
-with open('scripts/texts.csv', 'r') as f:
-    reader = csv.reader(f)
-    texts = list(reader)
-
 texts1 = [[1, 1, 'bla'], [1, 1, 'bla']]
+texts2 = [[1, 1, 'bla'], [2, 1, 'bla']]
 
 
-def test_countTextUsers_retutns_texts():
+def test_countTextUsers_retutns_1():
     assert countTextUsers(texts1) == [1]
+
+
+def test_countTextUsers_returns_1_and_2():
+    assert countTextUsers(texts2) == [1, 2]
