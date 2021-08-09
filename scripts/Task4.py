bfe9d87864bfcc166f42394ee7b302fe2041b4b1
Angelica <Angelica137@users.noreply.github.com>
2021-08-09 12:47:19 +0100

    .isTelemarketer returns numbers not in text list, green

--- scripts/Task4.py
+++ scripts/Task4.py
@@ -4,11 +4,11 @@ It's ok if you don't understand how to read files.
 """
 import csv
 
-with open('texts.csv', 'r') as f:
+with open('scripts/texts.csv', 'r') as f:
     reader = csv.reader(f)
     texts = list(reader)
 
-with open('calls.csv', 'r') as f:
+with open('scripts/calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
 
@@ -25,3 +25,34 @@ Print a message:
 The list of numbers should be print out one per line in lexicographic order with no duplicates.
 """
 
+
+def isTelemarketer(calls, texts):
+    telemarketers = []
+    not_telemarketers = []
+    for call in calls:
+        not_in_texts = True
+        for text in texts:
+            if call[0] in text:
+                not_in_texts = False
+                not_telemarketers.append(call[0])
+        if not_in_texts == True and call[0] not in telemarketers:
+            telemarketers.append(call[0])
+            print("these are the telemarketers " + str(telemarketers))
+    return telemarketers
+
+
+calls = [['1408371942', '98453 94494', '01-09-2016 06:01:12', '186'],
+         ['1408371333', '(022)28952819', '01-09-2016 06:01:59', '2093'],
+         ['97424 22395', '(022)47410783', '01-09-2016 06:03:51', '1975'],
+         ['93427 40118', '(080)33118033', '01-09-2016 06:11:23', '1156'],
+         ['(080)67362492', '(04344)316423', '01-09-2016 07:24:45', '2258']]
+
+
+texts = [['97424 22395', '97416 29480', '14-09-2016 20:52:17'],
+         ['93427 40118', '93422 47940', '14-09-2016 20:58:50'],
+         ['90356 11723', '81525 12041', '14-09-2016 20:59:30'],
+         ['90366 36573', '97425 12708', '14-09-2016 21:00:07'],
+         ['97424 53609', '97385 70012', '14-09-2016 21:01:01'],
+         ['93410 56456', '98453 86521', '14-09-2016 21:03:52']]
+
+print(isTelemarketer(calls, texts))
