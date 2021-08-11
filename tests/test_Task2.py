c2a151f57611e18589bd1b338577d40519dffc4a
Angelica <Angelica137@users.noreply.github.com>
2021-08-11 20:23:07 +0100

    amend task2

--- tests/test_Task2.py
+++ tests/test_Task2.py
@@ -11,6 +11,14 @@ calls2 = [['78130 00821', '98453 94494', '01-09-2016 06:01:12', '500'],
           ['97424 22395', '(022)47410783', '01-09-2016 06:03:51', '1975']]
 
 
+calls3 = [['78130 00821', '98453 94494', '01-09-2016 06:01:12', '500'],
+          ['78298 91466', '(022)28952819', '01-09-2016 06:01:59', '2093'],
+          ['(080)33251027', '98453 94494', '01-09-2016 06:01:12', '5000'],
+          ['97424 22395', '(022)47410783', '01-09-2016 06:03:51', '1975'],
+          ['97424 22395', '(022)47410783', '01-09-2016 06:03:51', '1975'],
+          ['(080)33251027', '98453 94494', '01-09-2016 06:01:12', '5000']]
+
+
 def test_longestCall_returns_calls1():
     assert longestCall(
         calls1) == "78298 91466 spent the longest time, 2093 seconds, on the phone during September 2016."
@@ -21,7 +29,6 @@ def test_longestCall_returns_calls2():
         calls2) == "97424 22395 spent the longest time, 3950 seconds, on the phone during September 2016."
 
 
-'''
-def test_longestCall_returns_10():
-    assert longestCall(calls) == '89071 50880'
-'''
+def test_longestCall_returns_calls2():
+    assert longestCall(
+        calls3) == "(080)33251027 spent the longest time, 10000 seconds, on the phone during September 2016."
