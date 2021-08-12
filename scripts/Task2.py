35f49ec41d4cb509715ff802e58a3e2d959480a5
Angelica <Angelica137@users.noreply.github.com>
2021-08-12 20:06:53 +0100

    delete obsolete

--- scripts/Task2.py
+++ scripts/Task2.py
@@ -30,24 +30,13 @@ def longest_call(calls):
     for call in calls:  # O(n)
         if call[0] in call_times:  # O(n)
             call_times[call[0]] += int(call[3])  # 1 step + O(n)
-            # print('a')
-            count += 1
         if call[1] in call_times:  # O(n)
             call_times[call[1]] += int(call[3])  # 1 step + O(n)
-            # print('b')
-            count += 1
         else:
             if call[0] not in call_times:  # O(n)
                 call_times[call[0]] = int(call[3])  # 1 step + O(n)
-                # print('c')
-                count += 1
             if call[1] not in call_times:
                 call_times[call[1]] = int(call[3])  # 1 step + O(n)
-                # print('d')
-                count += 1
-        #print("round " + str(count))
-        # print(call_times)
-
     max_time = max(call_times.values())  # O(n)
     number = [k for k, v in call_times.items() if v == max_time][0]  # O(n) + 1
     copy = "{} spent the longest time, {} seconds, on the phone during September 2016."  # 1 step
@@ -56,14 +45,3 @@ def longest_call(calls):
 
 
 print(longest_call(calls))
-
-
-calls4 = [['78130 00821', '98453 94494', '01-09-2016 06:01:12', '500'],
-          ['78298 91466', '(022)28952819', '01-09-2016 06:01:59', '2093'],
-          ['(022)47410783', '98453 94494', '01-09-2016 06:01:12', '3000'],
-          ['97424 22395', '(022)47410783', '01-09-2016 06:03:51', '1975'],
-          ['97424 22395', '(022)47410783', '01-09-2016 06:03:51', '1975'],
-          ['(080)33251027', '98453 94494', '01-09-2016 06:01:12', '5000']]
-
-
-print(longest_call(calls4))
