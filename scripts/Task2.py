abc088af5bd8b259413f0502c7c737e8f4360008
Angelica <Angelica137@users.noreply.github.com>
2021-08-13 11:40:31 +0100

    update task2 with big O analysis

--- scripts/Task2.py
+++ scripts/Task2.py
@@ -25,15 +25,16 @@ def longest_call(calls):
     '''
     returns the number that spent the longest time on the phone this period
     '''
-    telephone_nos = dict()
-    for i in range(len(calls)):
+    telephone_nos = dict()  # O(1)
+    for i in range(len(calls)):  # O(n)
         telephone_nos[calls[i][0]] = telephone_nos.get(
-            calls[i][0], 0) + int(calls[i][-1])
+            calls[i][0], 0) + int(calls[i][-1])  # O(1)
         telephone_nos[calls[i][1]] = telephone_nos.get(
-            calls[i][1], 0) + int(calls[i][-1])
-    max_telephone_num = max(telephone_nos, key=lambda x: telephone_nos.get(x))
-    max_duration = telephone_nos.get(max_telephone_num)
-    return max_telephone_num, max_duration
+            calls[i][1], 0) + int(calls[i][-1])  # O(1)
+    max_telephone_num = max(
+        telephone_nos, key=lambda x: telephone_nos.get(x))  # O(1)
+    max_duration = telephone_nos.get(max_telephone_num)  # O(1)
+    return max_telephone_num, max_duration  # O(1)
 
 
 task2_result = longest_call(calls)
